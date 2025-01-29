4ae3929e216c46c89f601a60b38896e63e95b646
logicallaw <logicallawbio@gmail.com>
2025-01-29 15:53:33 +0900

    chore: update AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly configService: ConfigService) { }
+  constructor(private readonly configService: ConfigService, private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
-    return this.configService.get<string>('DATABASE_HOST') ?? 'localhost';
+    return this.appService.getHello();
   }
 }
