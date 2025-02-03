bbe2c960aa9484bd5db940a9548c59190fb28de0
logicallaw <logicallawbio@gmail.com>
2025-02-03 18:10:13 +0900

    feat: add a endpoint of health check

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from 'src/app/app.service';
 
 @Controller()
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   getHome(): string {
     return 'Hello World!';
+  }
+
+  @Get('health')
+  check(@Res() res: Response) {
+    try {
+      return res.status(HttpStatus.OK).json({ message: 'health check에 성공하였습니다.' });
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: 'health check에 실패하였습니다.',
+        error: error.message,
+      });
+    }
   }
 }
