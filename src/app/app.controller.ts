523d25f475c9a90fe1ea151ec9e3e986bbafafe8
logicallaw <logicallawbio@gmail.com>
2025-02-12 13:45:08 +0900

    refactor(src/app): MVC 패턴 적용 및 불필요한 Notice Service 제거

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from 'src/app/app.service';
 
 @Controller()
@@ -8,18 +7,11 @@ export class AppController {
 
   @Get()
   getHome(): string {
-    return 'Hello World!';
+    return this.appService.getHome();
   }
 
   @Get('health')
-  check(@Res() res: Response) {
-    try {
-      return res.status(HttpStatus.OK).json({ message: 'health check에 성공하였습니다.' });
-    } catch (error) {
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: 'health check에 실패하였습니다.',
-        error: error.message,
-      });
-    }
+  check() {
+    return this.appService.healthCheck();
   }
 }
