5c8acd42c65322f68b2c0062f98ec1b2c8fe3784
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:17:57 +0900

    test: add test files
    - add major-style-notice-scheduler.service.spec.ts
    - add whole-notice-scheduler.service.spec.ts

--- src/notices/notice.module.ts
+++ src/notices/notice.module.ts
@@ -5,7 +5,7 @@ import { FirebaseService } from 'src/firebase/firebase.service';
 import { WholeNoticeScraperService } from 'src/notices/scraper/whole-notice-scraper.service';
 import { WholeNoticeSchedulerService } from 'src/notices/scheduler/whole-notice-scheduler.service';
 import { MajorStyleNoticeScraperService } from 'src/notices/scraper/major-style-notice-scraper.service';
-import { MajorStyleNoticeSchedulerService } from 'src/notices/scheduler/major-style-notice-scraper.service';
+import { MajorStyleNoticeSchedulerService } from 'src/notices/scheduler/major-style-notice-scheduler.service';
 
 @Module({
     providers: [
