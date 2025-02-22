f7bc1c46443b07736d86f92b51b5a60c7d261772
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:32:15 +0900

    docs(NoticeInterface): 주석 업데이트

--- src/notices/interfaces/notice.interface.ts
+++ src/notices/interfaces/notice.interface.ts
@@ -1,3 +1,16 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
+/**
+ * 공지 타입을 정의
+ */
 export interface Notice {
     id: string;
     title: string;
