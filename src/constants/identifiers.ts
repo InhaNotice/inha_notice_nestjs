3f258aa56a0a28891d2673ff1100329a720dfe21
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:39:41 +0900

    docs(IdentifierConstants): 주석 업데이트

--- src/constants/identifiers.ts
+++ src/constants/identifiers.ts
@@ -1,3 +1,16 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
+/**
+ * 이 클래스는 식별자를 정의합니다.
+ */
 export abstract class IdentifierConstants {
     static readonly UNKNOWN_ID: string = 'unknownId';
 };
