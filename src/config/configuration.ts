27903a0532d42862460ffeeaff6556192fcd4d17
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:40:02 +0900

    docs(configuration): 주석 업데이트

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,13 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
 export default () => {
     const majors: Record<string, { baseUrl: string; queryUrl: string }> = {};
 
