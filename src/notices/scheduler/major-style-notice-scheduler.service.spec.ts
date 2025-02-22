b5de1ac35cf1f0067e3d2931bfbf23b6428c4f76
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:30:48 +0900

    docs(MajorStyleNoticeSchedulerServiceSpce): 주석 업데이트

--- src/notices/scheduler/major-style-notice-scheduler.service.spec.ts
+++ src/notices/scheduler/major-style-notice-scheduler.service.spec.ts
@@ -1,3 +1,13 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { MajorStyleNoticeSchedulerService } from 'src/notices/scheduler/major-style-notice-scheduler.service';
 import { MajorStyleNoticeScraperService } from 'src/notices/scraper/major-style-notice-scraper.service';
