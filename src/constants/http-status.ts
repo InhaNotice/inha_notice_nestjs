27c1ccc5669db4fa7ce87ef626887d992283cdda
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:38:59 +0900

    docs(StatusCodeSettings): 주석 업데이트

--- src/constants/http-status.ts
+++ src/constants/http-status.ts
@@ -1,3 +1,16 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
+/**
+ * 이 클래스는 HTTP 상태 코드를 정의합니다.
+ */
 export abstract class StatusCodeSettings {
     static readonly STATUS_OKAY: number = 200;
 };
