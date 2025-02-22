b93c1ccae8dcaa89a9b37b665f09867e6fc121e3
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:36:54 +0900

    docs(FirebaseController): 주석 업데이트

--- src/firebase/firebase.controller.ts
+++ src/firebase/firebase.controller.ts
@@ -1,6 +1,25 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
 import { Controller, Post, Body } from '@nestjs/common';
 import { FirebaseService } from './firebase.service';
 
+/**
+ * FirebaseController
+ *
+ * 이 컨트롤러는 Firebase를 통한 알림 전송 관련 API 엔드포인트를 제공합니다.
+ * 
+ * ### 주요 기능:
+ * - POST /send-to-device
+ * - POST /send-to-major
+ */
 @Controller('notifications')
 export class FirebaseController {
   constructor(private readonly firebaseService: FirebaseService) { }
