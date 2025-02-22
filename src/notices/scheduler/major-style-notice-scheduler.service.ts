42b0113112ee5183203b8a226049837b6b043475
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:30:24 +0900

    docs(MajorStyleNoticeSchedulerService): 주석 업데이트

--- src/notices/scheduler/major-style-notice-scheduler.service.ts
+++ src/notices/scheduler/major-style-notice-scheduler.service.ts
@@ -1,3 +1,13 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
 import { Injectable, Logger, Scope } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { MajorStyleNoticeScraperService } from 'src/notices/scraper/major-style-notice-scraper.service';
