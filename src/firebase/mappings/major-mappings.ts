33bdb64cd7d153d6a848cce0b79345bfe03824f0
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:33:25 +0900

    docs(MajorMapping): 주석 업데이트

--- src/firebase/mappings/major-mappings.ts
+++ src/firebase/mappings/major-mappings.ts
@@ -1,3 +1,16 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
+/**
+ * 영문 학과명에서 국문 학과명으로 번역하는 컨테이너
+ */
 export const majorMappings: Record<string, string> = {
     'MECH': '기계공학과',
     'AEROSPACE': '항공우주공학과',
