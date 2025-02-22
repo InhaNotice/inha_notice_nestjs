581bf8ed8d1e34992089ead19422d16cfb93d644
logicallaw <logicallawbio@gmail.com>
2025-02-22 16:27:53 +0900

    docs(NoticeModule): 주석 업데이트

--- src/notices/notice.module.ts
+++ src/notices/notice.module.ts
@@ -1,3 +1,13 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-22
+ */
+
 import { Module } from '@nestjs/common';
 import { MajorNoticeScraperService } from 'src/notices/scraper/major-notice-scraper.service';
 import { MajorNoticeSchedulerService } from 'src/notices/scheduler/major-notice-scheduler.service';
