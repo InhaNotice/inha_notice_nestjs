fccae9e6f4bb0f4fef335ff9543667746b8d53d5
logicallaw <logicallawbio@gmail.com>
2025-02-23 22:47:33 +0900

    docs: 주석 업데이트

--- src/notices/scheduler/major-style-notice-scheduler.service.spec.ts
+++ src/notices/scheduler/major-style-notice-scheduler.service.spec.ts
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the root directory or at
  * https://opensource.org/license/mit
  * Author: junho Kim
- * Latest Updated Date: 2025-02-22
+ * Latest Updated Date: 2025-02-23
  */
 
 import { Test, TestingModule } from '@nestjs/testing';
