807677167ae6dd6cd7a3d23eda340d2224e3d0d1
logicallaw <logicallawbio@gmail.com>
2025-03-08 21:21:14 +0900

    refactor: FirebaseService 리팩터링

--- src/firebase/firebase.controller.ts
+++ src/firebase/firebase.controller.ts
@@ -18,7 +18,6 @@ import { FirebaseService } from 'src/firebase/firebase.service';
  * 
  * ### 주요 기능:
  * - POST /send-to-device
- * - POST /send-to-major
  */
 @Controller('notifications')
 export class FirebaseController {
@@ -27,18 +26,9 @@ export class FirebaseController {
   // 특정 디바이스로 알림
   @Post('send-to-device')
   async sendNotification(@Body() body: any): Promise<void> {
-    const { token, noticeTitle, data } = body;
+    const { deviceToken, noticeTitle, data } = body;
 
     // Firebase 푸시 알림 발송
-    await this.firebaseService.sendNotificationToDevice(token, noticeTitle, data);
-  }
-
-  // 학과 새로운 공지사항 알림
-  @Post('send-to-major')
-  async sendNotificationToAll(@Body() body: any): Promise<void> {
-    const { topic, noticeTitle, data } = body;
-
-    // Firebase 푸시 알림 발송 (전체 사용자)
-    await this.firebaseService.sendMajorNotification(noticeTitle, topic, data);
+    await this.firebaseService.sendNotificationToDevice(deviceToken, noticeTitle, data);
   }
 }
