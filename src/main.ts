b6505afb9e57d0b9a8bdf13e580feb15805e428e
logicallaw <logicallawbio@gmail.com>
2025-03-09 15:24:57 +0900

    chore: 불필요한 주석 삭제

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,20 @@ import * as morgan from 'morgan';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const logger = new Logger('Bootstrap'); // ✅ bootstrap 함수용 Logger 인스턴스 생성
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
   if (process.env.NODE_ENV == 'production') {
-    app.use(morgan('combined'));  // Production logging
+    app.use(morgan('combined'));
   } else {
-    app.use(morgan('dev'));  // Development logging
+    app.use(morgan('dev'));
   }
 
-  // NestFactory에서 app.get을 통하여 AppConfigService 주입받아 사용함
   const configService: ConfigService = app.get(ConfigService);
   const port = configService.get<number>('server.port') || 4000;
 
   await app.listen(port);
-  logger.log(`${port}번 포트에서 NestJS 서버 실행중!`); // ✅ bootstrap에서 직접 로깅
+  logger.log(`${port}번 포트에서 NestJS 서버 실행중!`);
 }
 bootstrap();
 
