d47b04e0479d91b7342aead3e8d0ac7958fdd699
logicallaw <logicallawbio@gmail.com>
2025-03-10 10:18:10 +0900

    chore: 업데이트 날짜 수정

--- src/notices/schedulers/absolute-style/major-notice-scheduler.service.ts
+++ src/notices/schedulers/absolute-style/major-notice-scheduler.service.ts
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the root directory or at
  * https://opensource.org/license/mit
  * Author: junho Kim
- * Latest Updated Date: 2025-03-08
+ * Latest Updated Date: 2025-03-10
  */
 
 import { Injectable, Logger, Scope } from '@nestjs/common';
