35a84917e3387792c836be025fa4d90a5e07316b
logicallaw <logicallawbio@gmail.com>
2025-05-17 15:42:04 +0900

    refactor: refactoring

--- src/notices/schedulers/absolute-style/major-style.scheduler.ts
+++ src/notices/schedulers/absolute-style/major-style.scheduler.ts
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the root directory or at
  * https://opensource.org/license/mit
  * Author: junho Kim
- * Latest Updated Date: 2025-05-16
+ * Latest Updated Date: 2025-05-17
  */
 
 import { Injectable, Logger, Scope } from '@nestjs/common';
