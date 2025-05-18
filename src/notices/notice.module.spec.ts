be70f62b52ce3ab6f2946176fe40a8835ca0dffe
logicallaw <logicallawbio@gmail.com>
2025-05-18 14:10:02 +0900

    chore: update comments

--- src/notices/notice.module.spec.ts
+++ src/notices/notice.module.spec.ts
@@ -1,3 +1,13 @@
+/*
+ * This is file of the project INGONG
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: junho Kim
+ * Latest Updated Date: 2025-05-18
+ */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigModule } from '@nestjs/config';
 import { NoticeModule } from './notice.module';
