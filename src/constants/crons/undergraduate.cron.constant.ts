79eef2d9232fae4df515ca1dfd7dcc2729805292
logicallaw <logicallawbio@gmail.com>
2025-07-05 17:42:54 +0900

    chore: 학사일정 크론식 수정
    - 기존(평일 한정) -> 현재(매일)

--- src/constants/crons/undergraduate.cron.constant.ts
+++ src/constants/crons/undergraduate.cron.constant.ts
@@ -5,18 +5,18 @@
  * For full license text, see the LICENSE file in the root directory or at
  * https://opensource.org/license/mit
  * Author: junho Kim
- * Latest Updated Date: 2025-05-19
+ * Latest Updated Date: 2025-07-05
  */
 
 /**
  * 학사일정 스케줄러의 Cron 상수를 정의한다.
  */
 export const UNDERGRADUATE_CRON: Record<string, string> = {
-    // 평일 18시에 하루 전 알림 보내기
-    CRON_DAY_BEFORE: '0 18 * * 1-5',
+    // 18시에 하루 전 알림 보내기
+    CRON_DAY_BEFORE: '0 18 * * *',
     TASK_DAY_BEFORE: '하루 전 일정(18시)',
 
-    // 평일 8시에 오늘 일정 알림 보내기
-    CRON_TODAY: '0 8 * * 1-5',
+    // 8시에 오늘 일정 알림 보내기
+    CRON_TODAY: '0 8 * * *',
     TASK_TODAY: '오늘 일정(8시)',
 } as const;
