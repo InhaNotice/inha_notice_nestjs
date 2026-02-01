5a27cdb0bfa13fe1487a87f11ecd382ccd161135
logicallaw <logicallawbio@gmail.com>
2026-02-01 22:32:29 +0900

    test(cron): update test codes

--- src/constants/crons/major.cron.constant.spec.ts
+++ src/constants/crons/major.cron.constant.spec.ts
@@ -5,14 +5,14 @@
  * For full license text, see the LICENSE file in the root directory or at
  * https://opensource.org/license/mit
  * Author: junho Kim
- * Latest Updated Date: 2025-05-18
+ * Latest Updated Date: 2026-02-01
  */
 
 import { MAJOR_CRON } from "src/constants/crons/major.cron.constant";
 
 describe('Major Cron 상수는', () => {
     it('정상적으로 배포환경의 상수로 정의되어 있다.', () => {
-        expect(MAJOR_CRON.CRON_WEEKDAYS).toBe('0 */10 9-16 * * 1-5');
+        expect(MAJOR_CRON.CRON_WEEKDAYS).toBe('0 3-59/10 9-16 * * 1-5');
         expect(MAJOR_CRON.CRON_DELETE_OLD).toBe('0 0 17 * * 1-5');
     });
 });
